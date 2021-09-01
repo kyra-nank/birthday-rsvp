e06d3dfb6c7d6fc5db48955ef2c48277b6fd5f6e
Kyra Nankivell <kyranankivell@Kyras-MacBook-Pro.local>
2021-09-01 17:48:46 -0400

    Update spots

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const attendeeSchema = new mongoose.Schema({
 // create new model
 const Attendee = mongoose.model('Attendee', attendeeSchema);
 
-var spotsRemaining = 165;
+var spotsRemaining = 163;
 
 // render home page
 app.get('/', function(req, res) {
